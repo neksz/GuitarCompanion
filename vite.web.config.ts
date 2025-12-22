34a599442b30be79af4bbc514bde4ec53cedf484
Patrik Czobor <czoborpatrik77@gmail.com>
2025-12-22 14:01:11 +0100

    Fix github actions

--- vite.web.config.ts
+++ vite.web.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/GuitarCompanion/',
   root: 'src/renderer',
   publicDir: 'public',
   build: {
